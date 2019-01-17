fe84964b3ecbcb7234baeed612fdeeb35c11cd0a
XSA_DEV <XSA_DEV@unknown>
2019-01-17 10:15:19 -0500

    Completed project

---
+++ web/resources/odataBasic/controller/App.controller.js
@@ -0,0 +1,63 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-use-before-define: 0, no-redeclare: 0, no-undef: 0*/
+//To use a javascript controller its name must end with .controller.js
+sap.ui.define([
+	"opensap/odataBasic/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
+	"use strict";
+
+	return BaseController.extend("opensap.odataBasic.controller.App", {
+
+		onInit: function() {
+			console.debug("Entered onInit function");
+			this.getView().addStyleClass("sapUiSizeCompact"); // make everything inside this View appear in Compact mode
+			var oConfig = this.getOwnerComponent().getModel("config");
+			var userName = oConfig.getProperty("/UserName");
+			var bpModel = this.getOwnerComponent().getModel("bpModel");
+			var oTable = this.getView().byId("bpTable");
+
+			//To-Do Bind Model to table
+			// oTable.setModel(bpModel);
+			// oTable.setEntitySet("BusinessPartners");
+			// oTable.setInitiallyVisibleFields("PARTNERID,COMPANYNAME,PARTNERROLE");
+			function fnLoadMetadata() {
+				console.debug("Entered fnLoadMetadata");
+				oTable.setModel(bpModel);
+				oTable.setEntitySet("BusinessPartners");
+				var oMeta = bpModel.getServiceMetadata();
+				var headerFields = "";
+				for (var i = 0; i < oMeta.dataServices.schema[0].entityType[0].property.length; i++) {
+					var property = oMeta.dataServices.schema[0].entityType[0].property[i];
+					headerFields +=  property.name + ",";
+				}
+				oTable.setInitiallyVisibleFields(headerFields);
+			}
+			console.debug("before attachMetadataLoaded function called");
+			bpModel.attachMetadataLoaded(bpModel, function() {
+				console.debug("attachMetadataLoaded anonymous function called");
+				fnLoadMetadata();
+			});
+			console.debug("after attachMetadataLoaded function called");
+			fnLoadMetadata();
+		},
+		onErrorCall: function(oError) {
+			if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
+				var errorRes = JSON.parse(oError.responseText);
+				if (!errorRes.error.innererror) {
+					sap.m.MessageBox.alert(errorRes.error.message.value);
+				} else {
+					if (!errorRes.error.innererror.message) {
+						sap.m.MessageBox.alert(errorRes.error.innererror.toString());
+					} else {
+						sap.m.MessageBox.alert(errorRes.error.innererror.message);
+					}
+				}
+				return;
+			} else {
+				sap.m.MessageBox.alert(oError.response.statusText);
+				return;
+			}
+
+		}
+	});
+});
